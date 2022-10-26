6326788ad8c5519901125162976d641a6231ec07
RoyalPC <o.metelev2020@yandex.ru>
2022-10-26 19:23:27 +0300

    Homework 13. Methods For Objects. Completed

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         Author author1 = new Author("George "," Orwell ");
-        Book book1 = new Book(" ", author1, 1949);
+        Book book1 = new Book(" 1984", author1, 1949);
 
         Author author2 = new Author("Star","  Gardt ");
         Book book2 = new Book("Мобилизованная нация ", author2, 1949);
 
         Author author3 = new Author("Лев ", "Толстой");
-        Book book3 = new Book("Война и мир", author3, 1863);
+        Book book3 = new Book(" Война и мир", author3, 1863);
 
         System.out.println(book1);
         System.out.println(book2);
